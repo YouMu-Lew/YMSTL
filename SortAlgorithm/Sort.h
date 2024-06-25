c38e54947602cd4d37fa83138c0352c542ab8a9b
悠木 <466040947@qq.com>
2024-06-25 14:30:42 +0800

    A add selectionSort and insertionSort

--- SortAlgorithm/Sort.h
+++ SortAlgorithm/Sort.h
@@ -1,46 +1,91 @@
 
 namespace ymstl {
-template <typename T>
-void swap(T& a, T& b) {
-    T temp = a;
-    a = b;
-    b = temp;
-}
+	template <typename T>
+	static void swap(T& a, T& b) {
+		T temp = a;
+		a = b;
+		b = temp;
+	}
 
-/// @brief 冒泡排序
-/// @tparam Iter
-/// @tparam Compare
-/// @param _begin
-/// @param _end
-/// @param comp
-template <typename Iter, typename Compare>
-void bubbleSort(Iter _begin, Iter _end, Compare comp) {
-    Iter right = _end;
-    while (right - _begin > 1) {
-        for (Iter i = _begin; i != right; i++) {
-            if (i == _begin)
-                continue;
-            if (!comp(*i, *(i - 1))) {
-                swap(*i, *(i - 1));
-            }
-        }
-        right--;
-    }
-}
+	const static auto less = [](auto a, auto b)->bool {return a < b; };
 
-template <typename Iter>
-void bubbleSort(Iter _begin, Iter _end) {
-    Iter right = _end;
-    while (right - _begin > 1) {
-        for (Iter i = _begin; i != right; i++) {
-            if (i == _begin)
-                continue;
-            if (*i < *(i - 1)) {
-                swap(*i, *(i - 1));
-            }
-        }
-        right--;
-    }
-}
+	/// @brief 冒泡排序
+	/// @tparam Iter
+	/// @tparam Compare
+	/// @param _begin begin iterator
+	/// @param _end end iterator
+	/// @param comp comp algotithm
+	template <typename Iter, typename Compare>
+	const static void bubbleSort(Iter _begin, Iter _end, Compare comp) {
+		Iter right = _end;
+		while (right - _begin > 1) {
+			for (Iter i = _begin; i != right; ++i) {
+				if (i == _begin)
+					continue;
+				if (comp(*i, *(i - 1))) {
+					swap(*i, *(i - 1));
+				}
+			}
+			right--;
+		}
+	}
 
+	template <typename Iter>
+	const static void bubbleSort(Iter _begin, Iter _end) {
+		bubbleSort(_begin, _end, less);
+	}
+
+	/// <summary>
+	/// 选择排序
+	/// </summary>
+	/// <typeparam name="Iter"></typeparam>
+	/// <typeparam name="Compare"></typeparam>
+	template <typename Iter, typename Compare>
+	const static void selectionSort(Iter _begin, Iter _end, Compare comp) {
+		Iter it = _begin, sel = _begin;
+		while (_begin != _end) {
+			while (it != _end) {
+				if (comp(*it, *sel)) {
+					sel = it;
+				}
+				++it;
+			}
+			swap(*_begin, *sel);
+			++_begin;
+			sel = _begin;
+			it = sel;
+		}
+	}
+
+	template <typename Iter>
+	const static void selectionSort(Iter _begin, Iter _end) {
+		selectionSort(_begin, _end, less);
+	}
+
+	/// <summary>
+	/// 插入排序
+	/// </summary>
+	/// <typeparam name="Iter"></typeparam>
+	/// <typeparam name="Compare"></typeparam>
+	/// <param name="_begin"></param>
+	/// <param name="_end"></param>
+	/// <param name="comp"></param>
+	template <typename Iter, typename Compare>
+	const static void insertionSort(Iter _begin, Iter _end, Compare comp) {
+		Iter sortedIt = _begin;
+		Iter cur = _begin + 1;
+		while (_end - sortedIt > 1) {
+			cur = sortedIt + 1;
+			while(comp(*cur, *(cur-1))) {
+				swap(*cur, *(cur - 1));
+				cur--;
+				if (cur == _begin)break;
+			}
+			++sortedIt;
+		}
+	}
+	template <typename Iter>
+	const static void insertionSort(Iter _begin, Iter _end) {
+		insertionSort(_begin, _end, less);
+	}
 };  // namespace ymstl
--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include "./SortAlgorithm/Sort.h"
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 
-void printV(std::vector<int>& v) {
-    for (int i : v)
-        std::cout << i << " ";
-    std::cout << std::endl;
-    return;
+template<typename T>
+void print(std::vector<T>& v, std::string pre = "") {
+	std::cout << pre << std::endl;
+	for (auto i : v)
+		std::cout << i << " ";
+	std::cout << std::endl;
+	return;
 }
 
 int main() {
-    std::vector<int> v = {1, 4, 6, 568, 678, 67, 45, 34, 5, -1};
-    printV(v);
-    ymstl::bubbleSort(v.begin(), v.end());
-    printV(v);
+
+	std::cout << "----------BubbleSort Test-----------" << std::endl;
+	std::vector<int> v1 = { 1, 4, 6, 568, 678, 67, 45, 34, 5, -1 };
+	print(v1, "v1:");
+	ymstl::bubbleSort(v1.begin(), v1.end(), std::greater<int>());
+	print(v1, "sorted by ymstl v1 £º");
+	sort(v1.begin(), v1.end(), std::less<int>());
+	print(v1, "sorted by sort v1 £º");
+	std::cout << "-------------------------------------" << std::endl;
+
+	std::vector<double> v2 = { 123.123,123.41,.4,34,.1234,.1,.41,24.,1.4,.123,4.13,4 };
+	print(v2, "v2: ");
+	ymstl::bubbleSort(v2.begin(), v2.end() - 1);
+	print(v2, "sorted by ymstl v2: ");
+	sort(v2.begin(), v2.end() - 1);
+	print(v2, "sorted by sort v2: ");
+	std::cout << "----------BubbleSort Test-----------" << std::endl;
+	std::cout << std::endl;
+
+	std::vector<int> v3 = { 1, 4, 6, 568, 678, 67, 45, 34, 5, -1 };
+	print(v3, "v3: ");
+	ymstl::selectionSort(v3.begin(), v3.end());
+	print(v3, "sorted by selectionSort v3: ");
+
+	std::vector<int> v4 = { 1, -4, 6, 568, 68, 67, 45, 34, 5, -1,0 };
+	print(v4, "v4: ");
+	ymstl::insertionSort(v4.begin(), v4.end());
+	print(v4, "sorted by insertionSort v4: ");
+
 }
