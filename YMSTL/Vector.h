78d825e5812c1ebf39839e4b84573443c4e83f6b
YouMu <lewyoumu@gmail.com>
2024-10-29 22:46:51 +0800

    refactor(Vector): move Vector implementation to YMSTL directory
    - Moved Vector.h from MySTL to YMSTL directory
    - Updated include guard and file contents accordingly
    - Changed namespace from ymstl to YMSTL
    - Improved code formatting and structure

--- YMSTL/Vector.h
+++ YMSTL/Vector.h
@@ -1,16 +1,130 @@
-#pragma once
+#ifndef _YMSTL_VECTOR_H_
+#define _YMSTL_VECTOR_H_
+
+#include <cstddef>
+#include <initializer_list>
+#include <utility>
+#include <stdexcept>
+#include <iostream>
+
 namespace ymstl {
+	template<typename Vector>
+	class VectorIterator {
+	public:
+		using iterator = VectorIterator;
+		using valueType = typename Vector::valueType;
+		using pointerType = valueType*;
+		using referenceType = valueType&;
+
+		VectorIterator() = default;
+		VectorIterator(pointerType ptr) :_ptr(ptr) {}
+
+		referenceType operator*()const noexcept {
+			return *_ptr;
+		}
+
+		const iterator& operator++() noexcept {
+			++_ptr;
+			return *this;
+		}
+
+		const iterator operator++(int) noexcept {
+			const iterator tempIt = *this;
+			++*this;
+			return tempIt;
+		}
+
+		bool operator==(const iterator& other) const noexcept {
+			return _ptr == other._ptr;
+		}
+
+		bool operator!=(const iterator& other) const noexcept {
+			return !(*this == other);
+		}
+
+		iterator operator+ (const std::ptrdiff_t off) {
+			iterator temp = *this;
+			temp._ptr += off;
+			return temp;
+		}
+
+		pointerType operator->() const noexcept {
+			return _ptr;
+		}
+
+	private:
+		pointerType _ptr;
+	};
+
 	template <typename T>
 	class Vector {
 	public:
+		using iterator = VectorIterator<Vector>;
+		using valueType = T;
+
 		Vector() { _reAllocate(2); }
+
+		Vector(size_t size, T value) {
+			_reAllocate(size);
+			for (size_t i = 0; i < size; i++) {
+				emplace_back(value);
+			}
+		}
+
+		Vector(const Vector& other) {
+			_reAllocate(other.size());
+			for (auto& element : other) {
+				emplace_back(element);
+			}
+		}
+
+		Vector(std::initializer_list<T> list) {
+			_reAllocate(list.size());
+			for (auto& element : list) {
+				push_back(std::move(element));
+			}
+		}
+
+		Vector& operator=(std::initializer_list<T> list) {
+			clear();
+			for (auto& element : list) {
+				push_back(std::move(element));
+			}
+			return *this;
+		}
+
+		Vector& operator=(const Vector& other) {
+			clear();
+			for (auto element : other) {
+				push_back(element);
+			}
+			return *this;
+		}
+
+		operator T* () {
+			return _data;
+		}
 
 		~Vector() {
 			clear();
+			::operator delete(_data, _capacity * sizeof(T));
 		}
 
-		T* begin() { return _data; }
-		T* end() { return _data + _size; }
+		iterator begin() noexcept {
+			return iterator(_data);
+		}
+
+		const iterator begin() const noexcept {
+			return iterator(_data);
+		}
+
+		iterator end() noexcept {
+			return iterator(_data + _size);
+		}
+
+		const iterator end() const noexcept {
+			return iterator(_data + _size);
+		}
 
 		constexpr size_t size() const { return _size; }
 
@@ -31,12 +145,12 @@ namespace ymstl {
 			return _data[index];
 		}
 
-		void push_back(T&& a) {
+		void push_back(T&& element) {
 			if (_size >= _capacity) {
 				_reAllocate(_capacity + _capacity / 2);
 			}
 			printf("push_back a new element by move\n");
-			_data[_size++] = std::move(a);
+			new(&_data[_size++]) T(std::move(element));
 		}
 
 		void push_back(const T& a) {
@@ -53,14 +167,42 @@ namespace ymstl {
 				_reAllocate(_capacity + _capacity / 2);
 			}
 			printf("emplace_back a new element\n");
+			/*
+			// 在已分配的内存上使用指定参数构造对象，此处的 new 关键字不负责分内存分配，而是 placement new 语法
+			// The `new` keyword in this context is not responsible for memory allocation; instead, it employs the `placement new` syntax to construct an object with specified parameters at already allocated memory. This operation takes place at the _size index of the _data array, where a T type object is instantiated using the arguments forwarded through `std::forward<Args>(args)...`.
+			*/
 			new(&_data[_size]) T(std::forward<Args>(args)...);
 			return _data[_size++];
 		}
 
 		void pop_back() {
 			if (_size > 0) {
+				printf("pop_back a element\n");
 				_data[--_size].~T();
 			}
+		}
+
+		iterator erase(iterator pos) {
+			if (pos == end()) {
+				return pos;
+			}
+			for (auto it = pos; it != end(); ++it) {
+				*it = *(it + 1);
+			}
+			pop_back();
+			return pos;
+		}
+
+		T* data() noexcept{
+			return _data;
+		}
+
+		const T* data() const noexcept{
+			return _data;
+		}
+
+		bool empty() const {
+			return _size == 0;
 		}
 
 		void clear() {
@@ -89,15 +231,19 @@ namespace ymstl {
 			if (newCapacity < _size)
 				_size = newCapacity;
 
-			T* newBlock = new T[newCapacity];
+			// allocate
+			T* newBlock = (T*)::operator new(newCapacity * sizeof(T));
 
-			// cpoy
+			// copy/move
 			for (size_t i = 0; i < _size; i++) {
-				newBlock[i] = std::move(_data[i]);
+				// 这里不能再用 = 赋值，使用 ::operator new 申请分配内存并没有调用 T 的构造函数，newBlock[i] 并不是一个 T 类型的值，需要调用 T 的构造函数
+				// Cannot use assignment here, as using ::operator new for memory allocation does not invoke the constructor of T; thus, newBlock[i] is not a value of type T, requiring the constructor of T to be called.
+				new(&newBlock[i]) T(std::move(_data[i]));
+				_data[i].~T();
 			}
 
-			// del
-			delete[] _data;
+			::operator delete(_data, _capacity * sizeof(T));
+
 			_data = newBlock;
 			newBlock = nullptr;
 			_capacity = newCapacity;
@@ -109,3 +255,5 @@ namespace ymstl {
 		size_t _size = 0, _capacity = 0;
 	};
 }  // namespace ymstl
+
+#endif // _YMSTL_VECTOR_H_
