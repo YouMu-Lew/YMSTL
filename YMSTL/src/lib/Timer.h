b6bf46872c76c8d9077ed81ca1363e6edff121c1
悠木 <466040947@qq.com>
2024-11-28 18:33:24 +0800

    refactor(Timer): 优化计时器类并支持自定义时间单位
    - 使用 steady_clock 替代 high_resolution_clock，提高时间测量准确性
    - 添加模板参数 Period，支持自定义时间单位
    - 实现显式构造函数，提高代码可读性
    - 删除拷贝和移动操作，防止不当使用导致错误
    - 优化成员变量声明，使用 const 修饰符提高代码安全性

--- YMSTL/src/lib/Timer.h
+++ YMSTL/src/lib/Timer.h
@@ -5,9 +5,11 @@
 #include <string>
 
 namespace ymstl {
+	template<typename Period = std::chrono::microseconds>
+		requires std::_Is_specialization_v<Period, std::chrono::duration>;
 	class timer {
 	public:
-		timer(const std::string& name = "Timer"):name_(name),start_point_(std::chrono::high_resolution_clock::now()){
+		explicit timer(const std::string& name = "Timer") :name_(name), start_point_(std::chrono::steady_clock::now()) {
 			std::cout << "Timer<" << name_ << "> start\n";
 		}
 
@@ -16,13 +18,19 @@ namespace ymstl {
 			std::cout << "Timer<" << name_ << "> end\n";
 		}
 
+		timer(const timer&) = delete;
+		timer(timer&&) = delete;
+
+		timer& operator=(const timer&) = delete;
+		timer& operator=(timer&&) = delete;
+
 		void split() const{
 			const auto end_point = std::chrono::high_resolution_clock::now();
-			const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_point - start_point_);
+			const auto duration = std::chrono::duration_cast<Period>(end_point - start_point_);
 			std::cout << "Timer<" << name_ << "> taken: " << duration << '\n';
 		}
 	private:
-		std::string name_;
-		std::chrono::time_point<std::chrono::high_resolution_clock> start_point_;
+		const std::string name_;
+		std::chrono::time_point<std::chrono::steady_clock> start_point_;
 	};
 }
