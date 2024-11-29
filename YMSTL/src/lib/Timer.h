391cc73f5d0893ceffbca76ffd9d6ce43e3661db
悠木 <466040947@qq.com>
2024-11-29 17:56:44 +0800

    refactor(Timer): 添加类模板文档注释  Add comments

--- YMSTL/src/lib/Timer.h
+++ YMSTL/src/lib/Timer.h
@@ -5,6 +5,13 @@
 #include <string>
 
 namespace ymstl {
+	/**
+	 * @brief Timer 类模板，用于测量代码块的执行时间。
+	 *
+	 * @tparam Period 时间单位，默认为 std::chrono::microseconds（微秒）。
+	 *
+	 * @requires Period 必须是 std::chrono::duration 的特化类型。
+	 */
 	template <typename Period = std::chrono::microseconds>
 		requires std::chrono::_Is_duration_v<Period>
 	class timer {
@@ -28,6 +35,7 @@ namespace ymstl {
 		timer& operator=(timer&&) = delete;
 
 		void split() const {
+			// 计时一次
 			const auto end_point = std::chrono::steady_clock::now();
 			const auto duration = std::chrono::duration_cast<Period>(end_point - start_point_);
 			std::cout << "Timer<" << name_ << "> taken: " << duration << '\n';
