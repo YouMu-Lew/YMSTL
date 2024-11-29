ebc4fc089519f678b7d1a4c311b9d056fa7bf888
悠木 <466040947@qq.com>
2024-11-29 17:48:39 +0800

    refactor(Timer): 优化 Timer 类的实现
    - 调整模板参数 Period 的约束条件
    - 使用 steady_clock 替代 high_resolution_clock

--- YMSTL/src/lib/Timer.h
+++ YMSTL/src/lib/Timer.h
@@ -5,11 +5,14 @@
 #include <string>
 
 namespace ymstl {
-	template<typename Period = std::chrono::microseconds>
-		requires std::_Is_specialization_v<Period, std::chrono::duration>;
+	template <typename Period = std::chrono::microseconds>
+		requires std::chrono::_Is_duration_v<Period>
 	class timer {
 	public:
-		explicit timer(const std::string& name = "Timer") :name_(name), start_point_(std::chrono::steady_clock::now()) {
+		//static_assert(std::chrono::_Is_duration_v<Period>, "Period should be duration.");
+
+		explicit timer(const std::string& name = "Timer")
+			: name_(name), start_point_(std::chrono::steady_clock::now()) {
 			std::cout << "Timer<" << name_ << "> start\n";
 		}
 
@@ -24,8 +27,8 @@ namespace ymstl {
 		timer& operator=(const timer&) = delete;
 		timer& operator=(timer&&) = delete;
 
-		void split() const{
-			const auto end_point = std::chrono::high_resolution_clock::now();
+		void split() const {
+			const auto end_point = std::chrono::steady_clock::now();
 			const auto duration = std::chrono::duration_cast<Period>(end_point - start_point_);
 			std::cout << "Timer<" << name_ << "> taken: " << duration << '\n';
 		}
